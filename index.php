01626153c2fc246b7d486929794e50c694adbb70
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-06-20 16:57:19 +0100

    text
    text

--- index.php
+++ index.php
@@ -8,6 +8,31 @@ include $tpl . 'nav.inc';
 
 <?php include $tpm . 'connect.php'; ?>
 
+<div class="container">
+<img src="https://source.unsplash.com/random" name="slide">
+</div>
+<script>
 
+var i = 0;
+var images = [
+              'https://images.pexels.com/photos/546226/pexels-photo-546226.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500',
+              'https://images.pexels.com/photos/1460670/pexels-photo-1460670.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500',
+              'https://images.pexels.com/photos/709496/pexels-photo-709496.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500'] ;
+var time = 2000;
+
+function changeImg(){
+
+document.slide.src = images[i];
+
+if (i < images.length - 1) {
+i++;
+} else {
+i = 0;
+}
+setTimeout("changeImg()", time);
+}
+window.onload = changeImg;
+
+</script>
 
 <?php include $tpl . 'footer.inc'; ?>
