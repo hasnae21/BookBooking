6daed25d4bbee21c30ac1f9251b5380527babab4
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-06-21 15:10:39 +0100

    r

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php 
 $title = 'Home';
+
 include 'route.php';
-// include $tpl . 'header.inc';
-include 'header.html';
 include $tpm . 'connect.php'; 
+// include $tpl . 'header.inc';
+
+include 'header.html';
+include 'nav.html';
+include 'body.html';
+
 // include $tpl . 'footer.inc';
 include 'footer.html';
+
 ?>
-<?php  ?>
